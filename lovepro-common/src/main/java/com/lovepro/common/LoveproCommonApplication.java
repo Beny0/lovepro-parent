3f0095ca1019865b89aef3c4ae05b83549429316
Beny0 <44239127+Beny0@users.noreply.github.com>
2019-12-01 19:00:36 +0800

    Add files via upload

---
+++ lovepro-common/src/main/java/com/lovepro/common/LoveproCommonApplication.java
@@ -0,0 +1,13 @@
+package com.lovepro.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveproCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveproCommonApplication.class, args);
+    }
+
+}
---
+++ lovepro-common/src/test/java/com/lovepro/common/LoveproCommonApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lovepro.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveproCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
